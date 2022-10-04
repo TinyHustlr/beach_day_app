7f222874a069151f631550635d5be49943fe8c02
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-10-04 17:07:31 +1100

    Google maps init

--- index.js
+++ index.js
@@ -9,9 +9,17 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Melbourne,AU&units=metr
             console.log(data)
             let tempHtml = data.main.temp
             let rainHtml = data.rain
+            console.log(rainHtml)
             let descriptionHtml = data.weather[0].description
             let weatherIconHtml = data.weather[0].icon
             temperatureEl.innerHTML = `Temperature: ${tempHtml}°C`
             rainEL.innerHTML = `Rain in last hour: ${rainHtml}mm`
-            weatherDescriptionEl.innerHTML = `Description: ${descriptionHtml}`
+            weatherDescriptionEl.innerHTML = `Description: ${descriptionHtml} <img src="https://openweathermap.org/img/w/${weatherIconHtml}.png">`
    })
+
+const googleMapsApiKey = "AIzaSyDn7pV_fa0zqGcvCDS0nIMbFl4VWTqixzQ"
+fetch("https://maps.googleapis.com/maps/api/staticmap?center=40.714%2c%20-73.998&zoom=12&size=400x400&key=" + googleMapsApiKey)
+   .then(response => response.json())
+   .then(data => console.log(data))
+   
+// requestAnimationFrame  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDn7pV_fa0zqGcvCDS0nIMbFl4VWTqixzQ&callback=initMap"></script>
